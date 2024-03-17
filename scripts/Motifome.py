47ab002a81306d68b5d0c281ea11026a811581ad
Klimov, Sergey <sklimov@carisls.com>
2024-03-17 16:32:17 -0700

    copyright adjust

--- scripts/Motifome.py
+++ scripts/Motifome.py
@@ -1,5 +1,5 @@
 ###############################################################
-### CopyrightÂ©2024. Caris MPI, Inc. All rights reserved.###
+### Copyright©2024. Caris MPI, Inc. All rights reserved.###
 ###############################################################
 
 import os
